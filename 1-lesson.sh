187061c486909adb0cfed08fd41c79470a3ad93a
xcellnt <goatbooster@outlook.com>
2023-02-02 19:46:05 +0700

    first commit

---
+++ 1-lesson.sh
@@ -0,0 +1,31 @@
+WHAT IS DevOps?
+
+ - A methodology that helps engineering teams build products better by coninuously getting user feedback
+ - backend the day software was developed in a factory, input would be programming and output would be product and sell it to users
+ - but since the advent of the internet, and have continuously updatable software, it's become really easy to launch things and get user feedback and integrate taht into the current product instead of making new version of the product
+ - and the idaea is really formalized by DevOps
+
+ DevOps Stage?
+  - Planning
+	you take a set of feature you wnat to build, and work with your team to make soem specifications for those features might look like
+  - Code
+	developer build the features
+  - Build
+	- for website you might take a source code and bundle it with JS
+	- for application you might make release for different OS 
+  - Test
+	- Automatic testing is CI ( Continuous Integration )
+	- Manual testing is QA ( Quality Assurance )
+  - Release
+	- take the software and send to customer
+  - Deploy
+	- accessible publicly by user
+  - Operating
+	- primaryly scaling making sure enough resources exist for the load
+	- adding more servers as required
+	- configuring things
+	- dealing with architecture problem
+  - Monitoring
+	- Make sure that those process are healty
+  - Finaly
+	- you take all feedback from ( user, operation, deplyoment, etc ) and you put back to the planing stage and repeat the cycle continuously
---
+++ readme.sh
@@ -0,0 +1,11 @@
+GOAL OF THIS COURSE
+
+learn fundamental DevOps concept for developer and eregular nginer practitioner
+
+COURSE OUTLINE
+
+Unit 1 = Code Review Automiation ( lesson 1-6 )
+Unit 2 = Deployment Strategies ( lesson 7-11 )
+Unit 3 = Application Performance Management ( lesson 12-13 )
+
+	4.26
